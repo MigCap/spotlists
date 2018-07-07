1334ec16c9f64b5cd764806890c950538285437b
Miguel C <MiguelC@iMac.local>
2018-07-07 22:38:17 +0200

    Second commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let defaultTextColor = '#fff';
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render () {
+    return (
+      <div>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    let name = 'Miguel'
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
