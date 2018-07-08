6ecc121a6a94b90af774f8f0e916c77ec0b3a2e7
Miguel C <MiguelC@iMac.local>
2018-07-08 02:07:12 +0200

    Added Filtering

--- src/App.js
+++ src/App.js
@@ -74,7 +74,8 @@ class Filter extends Component {
     return (
       <div>
         <img />
-        <input type="text" />
+        <input type="text" onKeyUp={event => 
+          this.props.onTextChange(event.target.value)}/>
       </div>
     );
   }
@@ -100,7 +101,10 @@ class Playlist extends Component {
 class App extends Component {
   constructor() {
     super();
-    this.state = {serverData: {}}
+    this.state = {
+      serverData: {},
+      filterString: ''
+    }
   }
   componentDidMount() {
     setTimeout(() => {
@@ -108,17 +112,20 @@ class App extends Component {
     }, 1000);
   }
   render() {
+
     return (
       <div className="App">
         {this.state.serverData.user ? 
           <div>
-            <h1 style={{...defaultStyle, 'font-size': '3em'}}>
+            <h1 style={{...defaultStyle, 'fontSize': '3em'}}>
               {this.state.serverData.user.name}'s Playlist
             </h1>
             <PlaylistCounter playlists={this.state.serverData.user.playlists} />
             <HoursCounter playlists={this.state.serverData.user.playlists} />
-            <Filter />
-            {this.state.serverData.user.playlists.map(playlist =>
+            <Filter onTextChange={text => this.setState({filterString: text})}/>
+            {this.state.serverData.user.playlists.filter(playlist =>
+              playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
+            ).map(playlist =>
               <Playlist playlist={playlist} />
             )}
           </div> : <h1 style={defaultStyle}>Loading...</h1> 
