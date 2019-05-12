3ce60a5ee7125a6812a9a5e0fee69b697ecbb404
Miguel C <MiguelC@iMac.local>
2019-05-12 14:39:20 +0200

    Changed layout. Added new navbar. Changed ReactJS to hooks version. Changed some requests services.

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import Login from '../../components/login/Login';
+//import Login from '../../components/login/Login';
+import LoginHook from '../../components/login/LoginHook';
 import Dashboard from '../../components/Dashboard/Dashboard';
+import Playlists from '../../components/Playlists/Playlists';
+import Header from '../../components/Header/Header';
 
 class App extends Component {
   render() {
@@ -12,9 +15,11 @@ class App extends Component {
       <BrowserRouter>
         <Fragment>
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path="/" component={LoginHook} />
             <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/playlists" component={Playlists} />
           </Switch>
+          <Header />
         </Fragment>
       </BrowserRouter>
     );
---
+++ src/components/login/LoginHook.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import Loader from '../Loader/Loader';
+
+import queryString from 'query-string';
+
+const LoginHook = () => {
+  const [accessToken, setAccessToken] = useState('');
+  const [isAuth, setIsAuth] = useState(false);
+  const [user] = useState('');
+
+  useEffect(() => {
+    const parsed = queryString.parse(window.location.search);
+    const token = parsed.access_token;
+
+    if (!token) return;
+
+    if (token && token !== '') {
+      window.localStorage.setItem('spotify_access_token', token);
+      setAccessToken(token);
+      setIsAuth(true);
+    }
+  }, []);
+
+  const getAuthToken = (isAuth, user) => {
+    if (user !== '' && isAuth) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/dashboard',
+            state: {
+              accessToken
+            }
+          }}
+        />
+      );
+    }
+
+    if (!isAuth) {
+      window.location = window.location.href.includes('localhost')
+        ? 'http://localhost:8888/login'
+        : 'https://kp-spotlist-backend.herokuapp.com/login';
+    }
+  };
+
+  if (!isAuth && accessToken === '') {
+    return (
+      <div className="text-center p-3 app-login pt-md-5">
+        <div className="container logo img-fluid pb-3">
+          <img src="spt-logo.png" alt="spotify logo" />
+          <hr />
+        </div>
+
+        <div className="container pt-4">
+          <h4 className="text-white">SPOTIFY PLAYLIST TRACKS</h4>
+        </div>
+
+        <div className="container mt-4 px-lg-5">
+          <h6 className="text-muted px-lg-5">
+            A simple react app which lists your playlist and the total amount of
+            hours of them, showing 3 top tracks from each one.
+          </h6>
+        </div>
+        <button
+          className="btn btn-success round-corner mt-5 px-5 font-weight-bold"
+          onClick={() => getAuthToken(isAuth, user)}>
+          LOGIN TO SPOTIFY
+        </button>
+      </div>
+    );
+  } else if (isAuth) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/dashboard',
+          state: {
+            accessToken
+          }
+        }}
+      />
+    );
+  } else if (!isAuth && accessToken === '') {
+    return <Loader />;
+  }
+};
+
+export default LoginHook;
